d8690b487c2d5f9dfc072bdec0e44538601ea36f
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2024-08-13 13:30:00 +0200

    Made SecurityTransparent.

--- Backport.System.Threading.Lock/Lock.cs
+++ Backport.System.Threading.Lock/Lock.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Security;
+[assembly: SecurityTransparent()]
 #if NET9_0_OR_GREATER
 [assembly: TypeForwardedTo(typeof(System.Threading.Lock))]
 #else
